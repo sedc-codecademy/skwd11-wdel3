c0fd4b70fcf64b1883712a3aed995d5a71694911
anetastankovska <aneta.stankovska@cw.bluedotcorp.com>
2023-10-14 19:05:38 +0200

    remove unnecessary modules

--- Class08-09-10-MoviesApp/FIMDB/src/app/features/movies/movies.module.ts
+++ Class08-09-10-MoviesApp/FIMDB/src/app/features/movies/movies.module.ts
@@ -19,7 +19,6 @@ import { CoreModule } from 'src/app/core/core.module';
     MoviesRoutingModule,
     MaterialModule,
     CoreModule,
-    // HttpClientModule
   ]
 })
 export class MoviesModule { }
